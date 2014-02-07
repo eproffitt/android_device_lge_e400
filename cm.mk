b0717fd85df928839004455f3eea3a42d03471fd
Shadow <eproffittibeta@gmail.com>
2014-02-07 11:30:39 -0700

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -4,19 +4,19 @@ $(call inherit-product, vendor/cm/config/gsm.mk)
 TARGET_BOOTANIMATION_NAME := vertical-240x320
 
 # Release name
-PRODUCT_RELEASE_NAME := OptimusL3
+PRODUCT_RELEASE_NAME := OptimusLogic
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/lge/e400/e400.mk)
+$(call inherit-product, device/lge/l35g/l35g.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=e0_tmo_eu BUILD_FINGERPRINT=lge/e0_tmo_eur/e0:2.3.6/GRK39F/V10c-MAR-28-2012.2ED8A66C75:user/release-keys PRIVATE_BUILD_DESC="e0_tmo_eur-user 2.3.6 GRK39F 2ED8A66C75 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=e0_tmo_eu BUILD_FINGERPRINT=lge/e0_trf_us/e0:2.3.6/GRK39F/V10f-JUL-20-2012.2ED8E240EE:user/release-keys PRIVATE_BUILD_DESC="e0_trf_ua-user 2.3.6 GRK39F 2ED8E240EE release-keys"
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := e400
-PRODUCT_NAME := cm_e400
+PRODUCT_DEVICE := l35g
+PRODUCT_NAME := cm_l35g
 PRODUCT_BRAND := lge
-PRODUCT_MODEL := LG-E400
+PRODUCT_MODEL := LG-L35G
 PRODUCT_MANUFACTURER := LGE
