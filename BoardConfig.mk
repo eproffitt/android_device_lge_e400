bab41d4a240bdf600bc95031fdcb726a73ad6ac2
Shadow <eproffittibeta@gmail.com>
2014-02-19 14:12:57 -0700

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -4,7 +4,7 @@ USE_CAMERA_STUB := true
 -include vendor/lge/l35g/BoardConfigVendor.mk
 
 TARGET_NO_BOOTLOADER := true
-TARGET_BOARD_PLATFORM := msm7k
+TARGET_BOARD_PLATFORM := msm7x27a
 TARGET_BOARD_PLATFORM_GPU := qcom-adreno200
 
 TARGET_CPU_ABI := armeabi-v7a
